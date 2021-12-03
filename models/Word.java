745a1ed2f4efb536d397413599e7e8d1df9ad137
Carlos <carlosmapache1@gmail.com>
2021-12-03 11:37:57 -0500

    asasa
    sasasa

--- models/Word.java
+++ models/Word.java
@@ -2,4 +2,18 @@ package models;
 
 public class Word {
     
+    private String word;
+
+    public Word(String word) {
+
+    }
+
+    public String getWord() {
+        return word;
+    }
+
+    public void setWord(String word) {
+        this.word = word;
+    }
+
 }
