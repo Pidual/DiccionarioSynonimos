32342e140327ebb6ffda06796c8355714832da14
Carlos <carlosmapache1@gmail.com>
2021-12-03 11:49:28 -0500

    SSS
    SSS

--- models/Library.java
+++ models/Library.java
@@ -3,8 +3,14 @@ package models;
 import java.util.ArrayList;
 
 public class Library {
+<<<<<<< Updated upstream
 
     ArrayList<Word> wordsList = new ArrayList();    
+=======
+    ArrayList<Word> diccionary = new ArrayList();
+    
+    
+>>>>>>> Stashed changes
 
     public Library(){
         wordsList = new ArrayList<>();
---
+++ views/SearchPanel.java
@@ -0,0 +1,33 @@
+package views;
+
+import javax.swing.JButton;
+import javax.swing.JTextField;
+import java.awt.*;
+import java.util.*;
+
+public class SearchPanel extends JPanel {
+
+    private JButton btnSearch;
+    private JTextField txtPropertyNumber;
+
+    public SearchPanel( ){
+        initComponents();
+    }
+
+    public void initComponents( ){ //Componentes del panel de busqueda
+        // this.setLayout(new GridBagLayout());
+        // GridBagConstraints gbc = new GridBagConstraints();
+        // btnSearch = new JButton("Buscar predio: ");
+        // btnSearch.setBackground(new Color(255, 248, 240));  //LE DA EL COLOR ROSADO AL BOTON
+        // add(btnSearch, gbc); // Gbc con valores default
+        // gbc.gridx = 1; // Grid x a 0
+        // gbc.gridy = 0; // Grid Y a 0
+        // txtPropertyNumber = new JTextField(15);
+        // add(txtPropertyNumber,gbc);
+    }
+
+    public String getPropertyNumber(){
+        return txtPropertyNumber.getText();
+    }
+
+}
