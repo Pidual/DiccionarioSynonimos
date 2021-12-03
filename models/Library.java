7bacc871bb059672ec2cce0c05556270f1ef5cb3
EricksitoUwU <erick.carreno@uptc.edu.co>
2021-12-03 11:52:54 -0500

    Update Library.
    Search Method

--- models/Library.java
+++ models/Library.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 
 public class Library {
 
-    ArrayList<Word> wordsList = new ArrayList();    
+    ArrayList<Word> wordsList = new ArrayList<>();    
 
     public Library(){
         wordsList = new ArrayList<>();
@@ -12,6 +12,16 @@ public class Library {
 
     public void addWord(Word word){
         wordsList.add(word);
+    }
+
+    public Word searchWord(String word){
+        Word searchedWord = null;
+        for(Word words : wordsList){
+            if(words.getWord().equals(word)){
+                searchedWord = words;
+            }
+        }
+        return searchedWord;
     }
 
     public void addSynonimous(String word, String synonimous){
