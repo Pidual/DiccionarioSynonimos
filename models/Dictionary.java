f877eefcf0825d4f47010f5fa241051a5ea42589
Carlos <carlosmapache1@gmail.com>
2021-12-07 08:36:38 -0500

    Mejoras a models Persistencia
    Persistencia Aumentada a eftCon 1

--- models/Dictionary.java
+++ models/Dictionary.java
@@ -4,35 +4,64 @@ import java.util.ArrayList;
 
 public class Dictionary {
 
-    ArrayList<Word> wordsList = new ArrayList<>();      
+    ArrayList<Word> wordsList = new ArrayList<>();
 
-    public Dictionary(){
+    public Dictionary() {
         wordsList = new ArrayList<Word>();
     }
 
-    public Word createWord(String addWord, ArrayList<String> words){
+    private Word createWord(String addWord, ArrayList<String> words) {
         Word word = new Word(addWord, words);
         return word;
     }
 
-    public void addSynonimous(String word, String synonimous){
-        
+    public void addSynonimous(String word, String synonimous) {
+
     }
 
-    public void addWord(String word){
-        wordsList.add(createWord(word,null));
+    public void addWord(String word, ArrayList<String> synonyms) {
+        wordsList.add(createWord(word, synonyms));
     }
 
-    public Word searchWord(String word){
+    /**
+     * Sobre carga del metodo addWord si algo se borra el de arriba
+     * crea una palabra anumaaa
+     * 
+     * @param partes
+     */
+    public void addWord(String[] partes) {
+        wordsList.add(createWord(partes[0], removeTheFirtsThenArrayList(partes)));       
+    }
+
+    /**
+     * Metodo Auxuliar para quitar el primer elemento de un Array y de paso
+     * convertirlo en una lista
+     * 
+     * @param partes
+     * @return una lista sin el primer elemento del array
+     */
+    private ArrayList<String> removeTheFirtsThenArrayList(String[] partes) {
+        ArrayList<String> synonyms = new ArrayList<String>();
+        for (int i = 1; i < partes.length; i++) {
+            synonyms.add(partes[i]);
+        }
+        return synonyms;
+    }
+
+    public Word searchWord(String word) {
         Word searchedWord = null;
-        for(Word words : wordsList){
-            if(words.getWord().equals(word)){
+        for (Word words : wordsList) {
+            if (words.getWord().equals(word)) {
                 searchedWord = words;
             }
         }
         return searchedWord;
     }
 
-   
+    public void testeo(){
+        for (Word palabrini : wordsList) {
+            System.out.println(palabrini.toString());
+        }
+    }
 
 }
--- presenter/presenter.java
+++ presenter/presenter.java
@@ -5,62 +5,56 @@ import views.DiccionaryView;
 import persitence.FileOperation;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.io.IOException;
 import java.util.ArrayList;
 
 
-public class Presenter implements ActionListener{
-    
+public class Presenter implements ActionListener {
+
     private DiccionaryView view;
     private Dictionary wikipedia;
     private FileOperation fileOperation;
     private ArrayList<String> wordAndSynonymsList;
 
-    public void run(){
+    public void run() {
         wikipedia = new Dictionary();
         view = new DiccionaryView(this);
         fileOperation = new FileOperation();
         load();
     }
-    
 
     @Override
-	public void actionPerformed(ActionEvent event) {
-		String command = event.getActionCommand();
+    public void actionPerformed(ActionEvent event) {
+        String command = event.getActionCommand();
         switch (command) {
             case "findTheSynonym":
-
+                wikipedia.testeo();
                 break;
             case "leftButtonPressed":
                 System.out.println("El boton << presionado");
-            break;
+                break;
             case "rigthButtonPressed":
                 System.out.println("El boton >> presionado");
-            break;
+                break;
             case "addWord":
                 System.out.println("Palabra activada");
-            break;
+                break;
             case "addSynonym":
                 System.out.println("Sinonimo activado");
-            break;
-            
+                break;
+
             default:
                 break;
         }
-	}
-
-    public void load(){ //RECIBE TODO DE FILEOPERATION Y EMPIEZA A CREAR PALABRAS
-        wordAndSynonymsList = fileOperation.loadTextFile("diccionarioDePalabras.txt");
-        for (String words : wordAndSynonymsList) {
-			System.out.println(words);
-		}
-		//un predio por partes
-		String[] partes = wordAndSynonymsList.get(0).split("\t");
-		for (int i = 0; i < partes.length; i++) {
-			System.out.println(partes[i]);
-            
-		}
-        
     }
 
+    /**
+     * Este metodo carga los archivos de texto
+     */
+    public void load() {
+        wordAndSynonymsList = fileOperation.loadTextFile("diccionarioDePalabras.txt");
+        for (int i = 0; i < wordAndSynonymsList.size(); i++) {
+            String [] partes = wordAndSynonymsList.get(i).split("\t");
+            wikipedia.addWord(partes);
+        }   
+    }
 }
